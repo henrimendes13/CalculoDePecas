c0cd1c1a450813b0350b0a2ac089079a12d2e5df
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-22 15:24:01 -0300

    Add project files.

---
+++ CalculoDePecas/Program.cs
@@ -0,0 +1,19 @@
+﻿
+while (true)
+{
+
+    int codpeca, numpecas;
+    double valoruni, valortotal;
+    string[] vet;
+
+    vet = Console.ReadLine().Split(' ');
+
+    codpeca = int.Parse(vet[0]);
+    
+   numpecas = int.Parse(vet[1]);
+    valoruni = double.Parse(vet[2]);
+    valortotal = numpecas * valoruni;
+    Console.WriteLine("VALOR A PAGAR: " + valortotal);
+     
+
+}
